dcd148245d1056195c676f7e729124f55306f5ec
SAADUMAR121 <112772287+SAADUMAR121@users.noreply.github.com>
2023-11-14 07:04:14 -0800

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 ≡ *DONATION*
-Share ABHISHEK-SER Bot Link And Support. Git Link https://github.com/AbhishekSuresh2/ABHISHEK-SER`
-let img = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'
+Share MR Bot group Link And Support. wp Link https://chat.whatsapp.com/Fc97ajkg5DvBbSGYfmRU8M`
+let img = 'https://graph.org/file/56c0fb63b1eb1b2ca789c.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
